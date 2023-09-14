34e71302cb490781fbd6da2518a6b03dc03712be
Butch Cortez <butchcortez.bmc@gmail.comm>
2023-09-14 15:04:49 +0800

    fix bg not loading in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        mobile: "url('./src/assetsimages/bg-sidebar-desktop.svg')",
-        desktop: "url('./src/assetsimages/bg-sidebar-desktop.svg')",
-        check: "url('./src/assetsimages/check.svg')",
+        mobile: "url('./assets/images/bg-sidebar-desktop.svg')",
+        desktop: "url('./assets/images/bg-sidebar-desktop.svg')",
+        check: "url('./assets/images/check.svg')",
       },
       borderWidth: {
         1: "1px",
